b478651709b37596cbb8e60e12a1e1d62097a6c1
nailoooon <nailoooon@mail.ru>
2022-05-28 11:52:35 +0600

    another

---
+++ src/go.mod
@@ -0,0 +1,3 @@
+module github.com/nailoooon/hw
+
+go 1.18
---
+++ src/main/main.go
@@ -0,0 +1,29 @@
+package src
+
+import "fmt"
+
+func fibonacci() func() int {
+  first := 0
+  second := 0
+  return func() int {
+    if first == 0 {
+      first = 1
+      second = 1
+      return 0
+    } else {
+      current := first
+      firstc := second
+      second = first + second
+      first = firstc
+      return current
+    }
+
+  }
+}
+
+func main() {
+  f := fibonacci()
+  for i := 0; i < 10; i++ {
+    fmt.Println(f())
+  }
+}
